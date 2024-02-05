99a00acc7a6b0968d33e9e484039dfffc196eabf
Sally-qasem <sallyqasem96@gmail.com>
2024-02-05 20:02:50 +0200

    my first commit

---
+++ src/QAClass/MySecondTask.java
@@ -0,0 +1,17 @@
+package QAClass;
+
+public class MySecondTask {
+    public static void main(String[] args) {
+    System.out.print("Sally");
+    System.out.print(" Qasem");
+    System.out.println();
+    System.out.println("Tira");
+    System.out.println();
+    System.out.print("1996");
+    System.out.println();
+    int myage = 27;
+            System.out.println(myage);
+            byte myVariable = 4;
+            System.out.println(myVariable);
+    }
+}
