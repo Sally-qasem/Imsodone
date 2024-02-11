20f8f289a42c3976a15ddf4f52f602fad7d4fbce
Sally-qasem <sallyqasem96@gmail.com>
2024-02-12 00:41:30 +0200

    CheckNumbers- HomeTask

---
+++ src/HomeTasks/CheckNumbers.java
@@ -0,0 +1,22 @@
+package HomeTasks;
+
+import java.util.Scanner;
+
+public class CheckNumbers {
+    public static void main(String[] args) {
+        int a = 0;
+        System.out.println(a % 2 == 0);
+
+        int b = 22;
+        System.out.println(b % 2 == 0);
+
+        int c = 55;
+        System.out.println(c % 2 == 0);
+
+        int d = 25;
+        System.out.println(d % 2 == 0);
+
+        int e = 10;
+        System.out.println(e % 2 == 0);
+    }
+}
