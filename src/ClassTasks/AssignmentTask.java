67a1475847537d825152e81464239c59bb188d1a
Sally-qasem <sallyqasem96@gmail.com>
2024-02-11 22:49:25 +0200

    AssignmentTask- ClassTask

---
+++ src/ClassTasks/AssignmentTask.java
@@ -0,0 +1,15 @@
+package ClassTasks;
+
+public class AssignmentTask {
+    public static void main(String[] args) {
+        int a = 10;
+        a = a + 90;
+        a = a - 25;
+        a = a / 3;
+        a = a % 10;
+        a *= (-3);
+        a = a + 1;
+        a = a - 1;
+        System.out.println("a = " + a); 
+    }
+}
