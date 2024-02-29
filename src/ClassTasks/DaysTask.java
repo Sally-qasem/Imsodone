111e483483093832c16ede73efdeb7e468841b76
Sally-qasem <sallyqasem96@gmail.com>
2024-02-29 15:38:20 +0200

    DaysTask- Class Task

---
+++ src/ClassTasks/DaysTask.java
@@ -0,0 +1,39 @@
+package ClassTasks;
+
+import java.util.Scanner;
+
+public class DaysTask {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Today is:");
+        int day = input.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("invalid");
+                break;
+
+        }
+    }
+}
