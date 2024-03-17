61c98bc1323f5dd943fcc84b38a50aec311d3aef
Sally-qasem <sallyqasem96@gmail.com>
2024-03-18 00:53:47 +0200

    CreditCardTask - Home Task Updated

--- src/HomeTasks/CreditCardTask.java
+++ src/HomeTasks/CreditCardTask.java
@@ -8,20 +8,23 @@ public class CreditCardTask {
         int cardnum = 12345678;
         String password = "abcd";
 
-        for (int i = 0; i < 5; i++);
-        System.out.println("Enter your Credit Card Number-");
-        int cardnumInput = input.nextInt();
-        input.nextLine();
-        System.out.println("Enter your password-");
-        String passwordInput = input.nextLine();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter your Credit Card Number- ");
+            int cardnumInput = input.nextInt();
+            input.nextLine();
+            System.out.println("Enter your password- ");
+            String passwordInput = input.nextLine();
 
-        if (cardnumInput == cardnum && passwordInput.equals(password)) {
-            System.out.println("Welcome to your account!");
-            return;
-        } else {
-            System.out.println("Something is wrong! Try again.");
-            System.out.println("You have" + (i) + "attempts left.");
+            if (cardnumInput == cardnum && passwordInput.equalsIgnoreCase(password)) {
+                System.out.println("Welcome to your account!");
+                return;
+            } else {
+                System.out.println("Something is wrong! Try again.");
+                System.out.println("You have " + (4 - i) + " attempts left.");
+            }
         }
+
         System.out.println("You have no attempts left. Your account is blocked.");
     }
 }
+
